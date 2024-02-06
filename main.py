d3c41ff5fd80d311e3e186b6b91746a1c918a79c
tuo20482 <143641615+tuo20482@users.noreply.github.com>
2024-02-05 19:22:12 -0500

    Added Typer integration. Added basic proof-of-concept functionality

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import spotipy
+import typer
 from spotipy.oauth2 import SpotifyClientCredentials
 
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+app = typer.Typer()
 
-ice_spice_uri = 'spotify:artist:3LZZPxNDGDFVSIPqf4JuEf'
+def uri_from_artist(name: str):
+    results = spotify.search(q="artist:" + name, type="artist")
+    items = results["artists"]["items"]
+    if len(items) > 0:
+        return items[0]["id"]
 
-results = spotify.artist_top_tracks(artist_id=ice_spice_uri, country="US")
-for track in results['tracks']:
-    print(track['name'])
+
+@app.command()
+def top_tracks(artist: str):
+    results = spotify.artist_top_tracks(artist_id=uri_from_artist(artist), country="US")
+    for track in results["tracks"]:
+        print(track["name"])
+
+
+if __name__ == "__main__":
+    app()
